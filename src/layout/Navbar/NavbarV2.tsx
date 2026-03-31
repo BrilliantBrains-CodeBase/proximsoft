df9de8e8ee8f44f9e668af67d7acae69e72d419f
Prashanth Naroju <prashanth.naro@gmail.com>
2026-03-31 12:14:52 +0530

    update the build errors in iconwith badge

--- src/layout/Navbar/NavbarV2.tsx
+++ src/layout/Navbar/NavbarV2.tsx
@@ -345,13 +345,13 @@ const MobileItem = ({ to, children, onClick }: any) => (
   </NavLink>
 );
 
-const IconWithBadge = ({ icon, count }: any) => (
-  <div className="relative">
-    <button className="w-10 h-10 rounded-full border flex items-center justify-center">
-      {icon}
-    </button>
-    <span className="absolute -top-1 -right-1 bg-yellow-400 text-xs w-5 h-5 flex items-center justify-center rounded-full">
-      {count}
-    </span>
-  </div>
-);
+// const IconWithBadge = ({ icon, count }: any) => (
+//   <div className="relative">
+//     <button className="w-10 h-10 rounded-full border flex items-center justify-center">
+//       {icon}
+//     </button>
+//     <span className="absolute -top-1 -right-1 bg-yellow-400 text-xs w-5 h-5 flex items-center justify-center rounded-full">
+//       {count}
+//     </span>
+//   </div>
+// );
